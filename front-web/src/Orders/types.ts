e1cf5428558ad7132acbe86a727afd9e84eac8c9
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2021-01-10 02:05:43 -0300

    Search order location

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -1,7 +1,15 @@
+import { type } from "os"
+
 export type Product = {
     id: number;
     name: string;
     price: number;
     description: string;
     imageUri: string;
+}
+
+export type OrderLocationdata = {
+    longitude: number;
+    address: string;
+    latitude: number;
 }
